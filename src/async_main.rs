cc9e0793c326d48c51a873de9e54b1c063460c3e
Peter Malmgren <ptmalmgren@gmail.com>
2021-06-03 16:02:22 -0400

    Add in async executor code

---
+++ src/async_main.rs
@@ -0,0 +1,40 @@
+mod timer_future;
+mod executor;
+
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll, Waker},
+    thread,
+    time::Duration,
+};
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake}
+};
+use executor::new_executor_and_spawner;
+use timer_future::TimerFuture;
+
+
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    // Spawn a task to print before and after waiting on a timer.
+    spawner.spawn(async {
+        println!("howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done!");
+    });
+
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
+    drop(spawner);
+
+    // Run the executor until the task queue is empty.
+    // This will print "howdy!", pause, and then print "done!".
+    executor.run();
+}
+
