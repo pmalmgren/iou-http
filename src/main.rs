a38066deb40c97272fbb091197968a7921de9764
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2021-06-03 17:08:16 -0400

    feat: add executor and basic reactor
    Co-authored-by: Peter Malmgren <pmalmgren@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 mod server;
+mod executor;
+mod reactor;
 
 use clap::{App, Arg};
 use pretty_env_logger;
-use server::Server;
+use reactor::Reactor;
+use std::thread;
+use std::sync::{Mutex, Arc};
+use std::net::TcpListener;
+use io_uring::{squeue::Entry, opcode, types::Fd};
+use std::os::unix::io::{AsRawFd, RawFd};
+use libc;
 
 use http::{Response, StatusCode};
 
 fn main() {
     pretty_env_logger::init();
 
-    let matches = App::new("iou-http")
-        .arg(
-            Arg::with_name("address")
-                .help("Address and port to bind to")
-                .default_value("localhost:8888")
-                .index(1)
-                .takes_value(true),
-        )
-        .get_matches();
-    let address = matches
-        .value_of("address")
-        .expect("Bind address is required");
+    let (mut reactor, sender) = reactor::Reactor::new().unwrap();
 
-    let mut server = Server::new(address, |_request| {
-        let body = "hello world".to_string();
-        Response::builder()
-            .status(StatusCode::OK)
-            .body(body)
-            .unwrap()
-    }).unwrap();
-    server.run().unwrap();
+    thread::spawn(move || {
+        reactor.run().unwrap();
+    });
+
+    let mut address = libc::sockaddr {
+        sa_family: 0,
+        sa_data: [0 as libc::c_char; 14],
+    };
+    let mut address_length: libc::socklen_t = std::mem::size_of::<libc::sockaddr>() as _;
+    println!("Address: {:p}", &address);
+    let socket = TcpListener::bind("0.0.0.0:8888").expect("bind");
+    let raw_fd = socket.as_raw_fd();
+    let entry = opcode::Accept::new(Fd(raw_fd), &mut address, &mut address_length)
+        .build();
+
+    let done: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    let done_clone = done.clone();
+    sender.send((entry, Box::new(move |n| {
+        println!("Received return {}", n);
+        let mut done = done_clone.lock().unwrap();
+        *done = true;
+    }))).unwrap();
+
+    loop {
+        if *done.lock().unwrap() {
+            break;
+        }
+    }
+    println!("Address: {:p}", &address);
 }
---
+++ src/reactor.rs
@@ -0,0 +1,84 @@
+use io_uring::{opcode, squeue::{PushError, Entry}, types::Fd, IoUring};
+use log::{debug, error, info};
+use std::collections::HashMap;
+use std::net::ToSocketAddrs;
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::{io, mem, net};
+use std::sync::mpsc::{Receiver, sync_channel, SyncSender};
+use httparse::{Request, EMPTY_HEADER, Error as HttpParseError};
+use http;
+
+use libc;
+use nix::sys::socket::{InetAddr, SockAddr};
+use thiserror::Error;
+// https://github.com/dtolnay/thiserror
+
+const AF_INET: u16 = libc::AF_INET as u16;
+const AF_INET6: u16 = libc::AF_INET6 as u16;
+const BUF_SIZE: usize = 512;
+const CHANNEL_BUFFER: usize = 10;
+
+#[derive(Error, Debug)]
+pub enum IouError {
+    #[error("Got invalid address family {0}")]
+    InvalidAddressFamily(u16),
+    #[error("Error submitting event to submission queue {0}")]
+    Push(#[from] PushError),
+    #[error("IO Error: {0}")]
+    Io(#[from] io::Error),
+}
+
+type Callback = Box<dyn FnOnce(i32) + Send + 'static>;
+
+pub struct Reactor {
+    ring: IoUring,
+    user_data: u64,
+    events: HashMap<u64, Callback>,
+    receiver: Receiver<(Entry, Callback)>,
+}
+
+impl Reactor {
+    pub fn new() -> Result<(Reactor, SyncSender<(Entry, Callback)>), IouError> {
+        let ring = IoUring::new(8)?;
+        let user_data = 1u64;
+        let events: HashMap<u64, Callback> = HashMap::new();
+        let (sender, receiver) = sync_channel(CHANNEL_BUFFER);
+        Ok((Reactor {
+            ring,
+            user_data,
+            events,
+            receiver
+        }, sender))
+    }
+
+    pub fn run(&mut self) -> Result<(), IouError> {
+        // TODO split submission and completion queues so they're owned
+        // by different threads
+        loop {
+            while let Ok((mut entry, callback)) = self.receiver.try_recv() {
+                entry = entry.user_data(self.user_data);
+                unsafe {
+                    self.ring.submission().push(&entry)?;
+                }
+                self.events.insert(self.user_data, callback);
+                self.user_data += 1;
+            }
+
+            self.ring.submitter().submit_and_wait(1)?;
+
+            for cqe in self.ring.completion() {
+                let ret = cqe.result();
+                let user_data = cqe.user_data();
+
+                if let Some(callback) = self.events.remove(&user_data) {
+                    (callback)(ret)
+                } else {
+                    error!(
+                        "got completion event from unknown submission: {}",
+                        user_data
+                    );
+                }
+            }
+        }
+    }
+}
