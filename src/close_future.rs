886ac9383d408d1ae8330ee203f4286fcfca9572
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2021-06-23 11:55:02 -0400

    feat: add close op
    Co-authored-by: Peter Malmgren <pmalmgren@users.noreply.github.com>

---
+++ src/close_future.rs
@@ -0,0 +1,18 @@
+use io_uring::{opcode, types::Fd};
+use std::net::TcpStream;
+use std::os::unix::io::IntoRawFd;
+use crate::syscall::SysCall;
+
+pub struct CloseFuture { }
+
+impl CloseFuture {
+    pub fn submit(socket: TcpStream) -> SysCall<CloseFuture> {
+		// TODO do we need to make sure the socket isn't dropped
+		// (because Rust will close the socket with a normal syscall
+		// if the value is dropped)
+        let entry =
+            opcode::Close::new(Fd(socket.into_raw_fd()))
+                .build();
+        SysCall::from_entry(entry, CloseFuture {})
+    }
+}
